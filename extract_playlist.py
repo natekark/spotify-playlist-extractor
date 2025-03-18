acd6010c6884e81eb2d11df093ef330d42be96fb
natekark <149103182+natekark@users.noreply.github.com>
2025-03-18 18:29:57 +0530

    Update extract_playlist.py

--- extract_playlist.py
+++ extract_playlist.py
@@ -2,14 +2,14 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 # Set up authentication
-client_id = 'cac301ee25ee41d9b36ec2fd0d77c4d1'  # Replace with your actual client ID
-client_secret = '2c2f9a56be37464dbdebcd0eb5883636'  # Replace with your actual client secret
+client_id = 'YOUR_CLIENT_ID'  # Replace with your actual client ID
+client_secret = 'YOUR_CLIENT_SECRET'  # Replace with your actual client secret
 
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 # Specify the playlist URL or ID
-playlist_id = '0UhnhjJ6nWkpmrBrRJP0c0'  # Replace with your actual playlist ID
+playlist_id = 'YOUR_PLAYLIST_ID'  # Replace with your actual playlist ID
 
 # Fetch the playlist data
 results = sp.playlist_tracks(playlist_id)
